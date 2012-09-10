9934b763fe5673523fe21b1f4e4063e7b2ba3a15
Diogo Benicá <diogobenica@gmail.com>
2012-09-10 09:43:26 -0300

    Verificando se a pasta storage existe antes de criar o arquivo

--- save.php
+++ save.php
@@ -1,5 +1,8 @@
 <?php
 $timeID = time(); 
+if(!is_dir('storage')){
+	mkdir('storage');
+}
 file_put_contents('storage/'.$timeID.'.txt',$_POST['stringJSON']); 
 echo json_encode(array('id'=>$timeID));
 ?>
